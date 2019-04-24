fce3c5b5a5b36ccc5652675dd42b070fbde18eb9
KareemPUNISHER <41842792+KareemPUNISHER@users.noreply.github.com>
2019-04-24 17:08:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,30 +9,30 @@ client.on('message', message => {
      
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(`:white_check_mark: ${argresult} تم تغيير الحالة بنجاح `)
+      message.channel.send(`Now I'm Playing ${argresult} :video_game:`)
   } else
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`:white_check_mark: تم تغيير الحالة بنجاح `)
+      message.channel.send(`Now I'm Watching ${argresult} :white_check_mark:`)
   } else
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`:white_check_mark: تم تغيير الحالة بنجاح`)
+      message.channel.send(`Now I'm Listening To ${argresult} :white_check_mark:`)
   } else
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/abdalkrim_badr");
-      message.channel.send(`:white_check_mark: تم تغيير الحالة بنجاح `)
+      message.channel.send(`Now I'm Streaming ${argresult} :purple_heart:`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`:white_check_mark: تم تغيير الحالة بنجاح `)
+      message.channel.send(`My Name Was Changed ${argresult} :white_check_mark:`)
 } else
 if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`:white_check_mark: تم تغيير الحالة بنجاح `);
+    message.channel.send(`Avatar Changed :white_check_mark:`);
 }
 });
  
